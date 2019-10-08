d75f21fe41dae61bd5d49aa48774d5f82c915945
Raphaël Franchet <raphael.franchet@ametys.org>
2019-10-08 18:36:08 +0200

    Ajouter un bouton "imprimer" dans la boite de dialogue imprimer #25

--- studio/resources/js/studio.js
+++ studio/resources/js/studio.js
@@ -3,14 +3,14 @@ var ConanStudio = {
         'fr': {
             'menu': "Studio",
             'printcardsLabel': "Imprimer",
-            'printcardsHint': "Sélectionnez les éléments à imprimer puis cliquez sur le bouton imprimer en haut à droite.",
+            'printcardsHint': "Sélectionnez les éléments à imprimer puis cliquez sur le bouton imprimer.",
             'printfaces': "Imprimer les dos sur des pages séparées",
             'printcardEmpty': "Vous n'avez sélectionné aucun élément à imprimer"
         },
         'en': {
             'menu': "Studio",
             'printcardsLabel': "Print",
-            'printcardsHint': "Select the items to print and then click on the top-right print button.",
+            'printcardsHint': "Select the items to print and then click on the print button.",
             'printfaces': "Print the backs on separated pages",
             'printcardEmpty': "You did not select any element to print"
         }
@@ -55,7 +55,9 @@ var ConanStudio = {
             "<div class=\"printcards\">"
             + "<p class='hint'>" + ConanStudio._i18n[Language].printcardsHint + "</p>"
             + "<p class='hint'><input type='checkbox' id='printback' onchange='$(this.parentNode.parentNode).toggleClass(\"back\")'><label for='printback'>" + ConanStudio._i18n[Language].printfaces + "</label></p>"
-            
+
+            + "<div class=\"print\"><button onclick=\"ConanStudio._printCards();\">" + ConanStudio._i18n[Language].printcardsLabel + "</button></div>"
+
             + "<h1>" + CardEquipment._i18n[Language].tab + "</h1>"
             + CardEquipment._getDisplayCardsCode(false)
             
@@ -68,13 +70,11 @@ var ConanStudio = {
             + "<h1>" + Tile._i18n[Language].tab + "</h1>"
             + "<div class=\"nocards\">" + Tile._i18n[Language].printnocard + "</div>"
             
+            + "<div class=\"print\"><button onclick=\"ConanStudio._printCards();\">" + ConanStudio._i18n[Language].printcardsLabel + "</button></div>"
+            
             + "</div>",
             null,
-            [{
-                label: ConanStudio._i18n[Language].printcardsLabel,
-                icon: "studio-icon-printcards",
-                fn: "ConanStudio._printCards();"
-            }]
+            []
         );
     },
     
