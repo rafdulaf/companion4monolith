c76fb49225d70276a36f708f66b4f0fa55816a5a
Raphaël Franchet <raphael.franchet@ametys.org>
2019-12-13 12:22:54 +0100

    Affichage Livre de règles Héros #39

--- rules/resources/js/autozoom.js
+++ rules/resources/js/autozoom.js
@@ -1,5 +1,7 @@
 function autoZoom() {
-    document.body.style.zoom = Math.floor(window.innerWidth / 909.0 * 100.0) / 100.0; 
+    document.body.style.transform = "scale(" + Math.floor(window.innerWidth / 909.0 * 100.0) / 100.0 + ")";
+    document.body.style.transformOrigin = "top left";
+    document.body.style.overflow = "hidden";
 }
 window.onresize = autoZoom;
 autoZoom();
