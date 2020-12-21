a1bfae4b3e2f2c918adce486778d0a4c2c7535c9
Raphaël Franchet <raphael.franchet@ametys.org>
2020-12-21 12:28:23 +0100

    Refactor code in order to open to Batman #94

---
+++ Conan/general/default.js
@@ -0,0 +1,4 @@
+// Preferences
+Extensions = JSON.parse(localStorage.getItem("Extensions")) || {
+    'custom-box': 'king', 'stygia': true, 'nordheim': true, 'khitai': true
+};
--- _common/general/utils.js
+++ _common/general/utils.js
@@ -3,7 +3,12 @@ Utils = {
         if (navigator.language == 'fr' || navigator.language.indexOf('fr-') == 0) return 'fr';
         else if (navigator.language == 'it' || navigator.language.indexOf('it-') == 0) return 'it';
         else return 'en'; 
-    },
+    }
 }
 
 Language = localStorage.getItem("Language") || Utils.autodetectLanguage();
+
+//var LazyImage = " loading=\"lazy\"";
+//if (/firefox/.test(navigator.userAgent.toLowerCase())) LazyImage = ""; // Not working correctly on FF75/FF76
+var LazyImage = "";
+                    
