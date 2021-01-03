9e8ec2600fdd19aa261aef099e3221f4a876b84b
Raphaël Franchet <raphael.franchet@ametys.org>
2021-01-03 12:04:46 +0100

    Refactor code in order to open to Batman #94

--- Batman/general/default.js
+++ Batman/general/default.js
@@ -1,4 +1,4 @@
 // Preferences
 Extensions = JSON.parse(localStorage.getItem(Application + "_Extensions")) || {
-    // default values here
+    'box': 'corebox', 'arkham-asylum': true, 'wayne-manor': true, 'maps-pack': true
 };
