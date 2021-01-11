45230d4d6e454556c898eba4245a6fdacef22654
madcollectordev <34870193+madcollectordev@users.noreply.github.com>
2021-01-11 18:16:31 +0100

    Faute

--- Batman/about/about.js
+++ Batman/about/about.js
@@ -4,10 +4,10 @@ About = mergeObject(About, {
             'smallName': "Batman",
             'name': "Compagnon<br/>de Batman",
             'title': "Le compagnon de Batman",
-            'text': "'Compagnion de Batman' est une application indépendante pour accompagner les joueurs dans le jeu 'Batman Gotham Cithy Chronicles'.<h1>Important</h1>Cette application n'a aucun lien avec l'éditeur du jeu Batman Gotham City Chronicles : Monolith.<br/>Elle a été réalisée par <a href=\"https://the-overlord.com/index.php?/profile/5567-madcollector/\" target='_blank'>@madcollector</a> et <a href='https://the-overlord.com/index.php?/profile/5240-cochon/' target='_blank'>@cochon</a> en s'appuyant sur l'aide de la communauté de <a href='https://the-overlord.com'>The Overlord</a> et divers outils et ressources existantes.",
+            'text': "'Compagnon de Batman' est une application indépendante pour accompagner les joueurs dans le jeu 'Batman Gotham Cithy Chronicles'.<h1>Important</h1>Cette application n'a aucun lien avec l'éditeur du jeu Batman Gotham City Chronicles : Monolith.<br/>Elle a été réalisée par <a href=\"https://the-overlord.com/index.php?/profile/5567-madcollector/\" target='_blank'>@madcollector</a> et <a href='https://the-overlord.com/index.php?/profile/5240-cochon/' target='_blank'>@cochon</a> en s'appuyant sur l'aide de la communauté de <a href='https://the-overlord.com'>The Overlord</a> et divers outils et ressources existantes.",
             'copyright-text': "'Batman' est un jeu de plateau de figurines édité par <a href='http://monolithedition.com' target='_blank'>Monolith</a>.<br/>"
                     + "Batman et tous les personnages associés sont la propriété de DC. Le logo Batman Gotham Cithy Chronicles et les polices de caractères utilisées sont la propriété de Monolith.<br/>"
-                    + "L'image du 'Compagnion de Batman' a été créée à partir du logo du jeu Batman Gotham Cithy Chronicles.<br/>",
+                    + "L'image du 'Compagnon de Batman' a été créée à partir du logo du jeu Batman Gotham Cithy Chronicles.<br/>",
             'tolink': "https://the-overlord.com/index.php?/topic/4360-application-de-compagnon-de-jeu-pour-batman/"
         },
         'en': {
