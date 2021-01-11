dcb902204e8f0bf39ed8ec7c5f7a6b570d76fc29
madcollectordev <34870193+madcollectordev@users.noreply.github.com>
2021-01-11 09:35:39 +0100

    Chargement des Skills

--- Batman/encyclopedia/encyclopedia.js
+++ Batman/encyclopedia/encyclopedia.js
@@ -5,6 +5,7 @@ var Encyclopedia = {
             Utils.loadJSON("data/expansions.json")  .then(function(data) { Encyclopedia.expansions = data; }),
             Utils.loadJSON("data/maps.json")        .then(function(data) { Encyclopedia.maps = data; }),
             Utils.loadJSON("data/rules.json")       .then(function(data) { Encyclopedia.rules = data; })
+            Utils.loadJSON("data/skills.json")       .then(function(data) { Encyclopedia.skills = data; })
         ]);
     }
 };
