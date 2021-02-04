985daca8fab14789ce5b902d33f926fdb3765490
Raphaël Franchet <raphael.franchet@ametys.org>
2021-02-04 09:38:41 +0100

    full batman name

--- Batman/about/about.js
+++ Batman/about/about.js
@@ -1,20 +1,20 @@
 About = mergeObject(About, {
     _i18n: {
         'fr': {
-            'smallName': "Batman",
-            'name': "Compagnon<br/>de Batman",
-            'title': "Le compagnon de Batman",
-            'text': "'Compagnon de Batman' est une application indépendante pour accompagner les joueurs dans le jeu 'Batman Gotham Cithy Chronicles'.<h1>Important</h1>Cette application n'a aucun lien avec l'éditeur du jeu Batman Gotham City Chronicles : Monolith.<br/>Elle a été réalisée par <a href=\"https://the-overlord.com/index.php?/profile/5567-madcollector/\" target='_blank'>@madcollector</a> et <a href='https://the-overlord.com/index.php?/profile/5240-cochon/' target='_blank'>@cochon</a> en s'appuyant sur l'aide de la communauté de <a href='https://the-overlord.com'>The Overlord</a> et divers outils et ressources existantes.",
+            'smallName': "Batman GCC",
+            'name': "Compagnon<br/>de Batman GCC",
+            'title': "Le compagnon de Batman GCC",
+            'text': "'Compagnon de Batman GCC' est une application indépendante pour accompagner les joueurs dans le jeu 'Batman Gotham Cithy Chronicles'.<h1>Important</h1>Cette application n'a aucun lien avec l'éditeur du jeu Batman Gotham City Chronicles : Monolith.<br/>Elle a été réalisée par <a href=\"https://the-overlord.com/index.php?/profile/5567-madcollector/\" target='_blank'>@madcollector</a> et <a href='https://the-overlord.com/index.php?/profile/5240-cochon/' target='_blank'>@cochon</a> en s'appuyant sur l'aide de la communauté de <a href='https://the-overlord.com'>The Overlord</a> et divers outils et ressources existantes.",
             'copyright-text': "'Batman Gotham Cithy Chronicles' est un jeu de plateau de figurines édité par <a href='http://monolithedition.com' target='_blank'>Monolith</a>.<br/>"
                     + "Batman et tous les personnages associés sont la propriété de DC. Le logo Batman Gotham Cithy Chronicles et les polices de caractères utilisées sont la propriété de Monolith.<br/>"
                     + "L'image du 'Compagnon de Batman' a été créée à partir du logo du jeu Batman Gotham Cithy Chronicles.<br/>",
             'tolink': "https://the-overlord.com/index.php?/topic/4360-application-de-compagnon-de-jeu-pour-batman/"
         },
         'en': {
-            'smallName': "Batman",
-            'name': "Companion<br/>to Batman",
-            'title': "The companion to Batman",
-            'text': "'Companion to Batman' is an independant application to help the players in the game 'Batman Gotham Cithy Chronicles'.<h1>Disclamer</h1>This application has no link with the producer of the game Batman Gotham City Chronicles: Monolith.<br/>It was realized by <a href=\"https://the-overlord.com/index.php?/profile/5567-madcollector/\" target='_blank'>@madcollector</a> and <a href='https://the-overlord.com/index.php?/profile/5240-cochon/' target='_blank'>@cochon</a> relying the community of <a href='https://the-overlord.com'>The Overlord</a> and several existing tools or resources.",
+            'smallName': "Batman GCC",
+            'name': "Companion<br/>to Batman GCC",
+            'title': "The companion to Batman GCC",
+            'text': "'Companion to Batman GCC' is an independant application to help the players in the game 'Batman Gotham Cithy Chronicles'.<h1>Disclamer</h1>This application has no link with the producer of the game Batman Gotham City Chronicles: Monolith.<br/>It was realized by <a href=\"https://the-overlord.com/index.php?/profile/5567-madcollector/\" target='_blank'>@madcollector</a> and <a href='https://the-overlord.com/index.php?/profile/5240-cochon/' target='_blank'>@cochon</a> relying the community of <a href='https://the-overlord.com'>The Overlord</a> and several existing tools or resources.",
             'copyright-text': "'Batman Gotham Cithy Chronicles' is a board game published by <a href='http://monolithedition.com' target='_blank'>Monolith</a>.<br/>"
                     + "Batman and all associated characters are the property of DC. The Batman Gotham Cithy Chronicles logo and the fonts used are the property of Monolith.<br/>"
                     + "The image of 'Companion to Batman' was created by adapting the Batman Gotham Cithy Chronicles logo.<br/>",
