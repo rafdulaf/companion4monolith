87f0fafd50b7a7d206b69387b041f5c68b0be6a4
Raphaël Franchet <raphael.franchet@ametys.org>
2021-02-24 19:18:17 +0100

    Generalize studio and Move the + button #129

---
+++ engine/studio/studio.js
@@ -0,0 +1,110 @@
+var Studio = {
+      _i18n: {
+        'fr': {
+            'menu': "Studio",
+            'printcardsLabel': "Imprimer",
+            'printcardsHint': "Sélectionnez les éléments à imprimer puis cliquez sur le bouton imprimer.",
+            'printfaces': "Imprimer les dos sur des pages séparées",
+            'printmargin': "Imprimer avec les marges pour la découpe",
+            'printcardEmpty': "Vous n'avez sélectionné aucun élément à imprimer"
+        },
+        'en': {
+            'menu': "Studio",
+            'printcardsLabel': "Print",
+            'printcardsHint': "Select the items to print and then click on the print button.",
+            'printfaces': "Print the backs on separated pages",
+            'printmargin': "Print with cut margins",
+            'printcardEmpty': "You did not select any element to print"
+        },
+        'it': {
+            'menu': "Studio",
+            'printcardsLabel': "Stampa",
+            'printcardsHint': "Seleziona gli oggetti da stampare e clicca su Stampa.",
+            'printfaces': "Stampa i retri su pagine separate",
+            'printmargin': "Stampa gli indicatori di taglio",
+            'printcardEmpty': "Non hai selezionato nessun elemento da stampare"
+            }
+    },
+
+    _currentSlide: null,
+    _slides: [],
+    
+    _loadItems: function() {
+        Studio._items = [];
+    },
+
+    init: function() {
+        Studio._loadItems();
+        if (Studio._items.length == 0)
+        {
+            return;
+        }
+
+        Nav.addIcon(Studio._i18n[Language].menu, "studio-icon", "studio");
+        
+        Studio._items.forEach(item => item.preinit());
+
+        Nav.createTabs('studio', Studio._slides, Studio.onChange);
+
+        Studio._items.forEach(item => item.init());
+
+        Studio.onChange();
+
+        let copyright = "";
+        Studio._items.forEach(item => copyright += item.copyright());
+        About.addCopyright(Studio._i18n[Language].menu, copyright);
+    },
+
+    onChange: function(event, slick) {
+        var slide = slick && slick.currentSlide || 0;
+
+        if (Studio._currentSlide != null)
+            Studio._slides[Studio._currentSlide].onHide();
+
+        Studio._currentSlide = slide;
+        Studio._slides[Studio._currentSlide].onShow();
+    },
+
+    printCards: function()
+    {
+        let studiosCode = "";
+        Studio._items.forEach(item => studiosCode += "<h1>" + item._i18n[Language].tab + "</h1>" + item.printCode());
+        
+        Nav.dialog(Studio._i18n[Language].printcardsLabel,
+            "<div class=\"printcards back cut\">"
+
+            + "<p class='hint'>" + Studio._i18n[Language].printcardsHint + "</p>"
+            + "<p class='hint'><input type='checkbox' id='printback' checked='checked'  onchange='$(this.parentNode.parentNode).toggleClass(\"back\")'><label for='printback'>" + Studio._i18n[Language].printfaces + "</label></p>"
+            + "<p class='hint'><input type='checkbox' id='printcut' checked='checked' onchange='$(this.parentNode.parentNode).toggleClass(\"cut\")'><label for='printcut'>" + Studio._i18n[Language].printmargin + "</label></p>"
+
+            + "<div class=\"print\"><button onclick=\"Studio._printCards();\">" + Studio._i18n[Language].printcardsLabel + "</button></div>"
+
+            + studiosCode
+
+            + "<div class=\"print\"><button onclick=\"Studio._printCards();\">" + Studio._i18n[Language].printcardsLabel + "</button></div>"
+
+            + "<div class=\"newpage\"></div>"
+
+            + "</div>",
+            null,
+            []
+        );
+        // share back of spell and equipment
+        $(".newpage").append($(".printcards .back"));
+    },
+
+    _printCards: function()
+    {
+        if ($(".printcards > ~input:checked").length == 0)
+        {
+            About.warnToast(Studio._i18n[Language].printcardEmpty);
+            return;
+        }
+        else
+        {
+            window.print();
+        }
+    }
+};
+
+Utils._toInitialize.push(Studio.init);
