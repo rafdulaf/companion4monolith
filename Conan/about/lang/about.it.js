c8192f6d1e4ecc3829ba488f11c2378bdb2f50bc
Raphaël Franchet <raphael.franchet@ametys.org>
2021-06-02 21:44:04 +0200

    Put Conqueror stuff back Italian translations before next release #128

--- Conan/about/lang/about.it.js
+++ Conan/about/lang/about.it.js
@@ -2,7 +2,7 @@ About._i18n = mergeObject(About._i18n, {
     'smallName': "Conan",
     'name': "Conan<br/>Companion",
     'title': "Conan Companion",
-    'text': "'Conan Companion' è un'applicazione indipendente di supporto per i giocatori del gioco 'Conan di Monolith'.<h1>Importante</h1>Questa applicazione è fatta da fans con l'autorizzazione di Monolith.",
+    'text': "'Conan Companion' è un'applicazione indipendente di supporto per i giocatori del gioco 'Conan di Monolith'.<h1>Importante</h1>Questa applicazione è fatta da fans con l'autorizzazione di <a href='https://www.facebook.com/frederic.henry1/'>Frédéric Henry</a> e il supporto di Monolith (produttore del gioco di Conan).<br/>Realizzata da <a href='https://the-overlord.com/index.php?/profile/5240-cochon/' target='_blank'>@cochon</a> facendo affidamento alla comunità di <a href='https://the-overlord.com'>The Overlord</a> e usando numerosi tools esistenti e risorse.",
     'copyright-text': "'Conan' è un gioco da tavolo della <a href='http://monolithedition.com' target='_blank'>Monolith</a>.<br/>"
             + "Il logo di Conan e i fonts usati sono proprietà della Monolith.<br/>"
             + "Le immagini di 'Conan Companion' sono state create adattando il logo di Conan.<br/>",
