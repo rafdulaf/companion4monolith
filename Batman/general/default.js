17675436f14769b626e49e60255415673b7527db
Raphaël Franchet <raphael.franchet@ametys.org>
2022-03-27 15:26:03 +0200

    Batman : règles par défaut #143

--- Batman/general/default.js
+++ Batman/general/default.js
@@ -1,4 +1,4 @@
 // Preferences
 Extensions = JSON.parse(localStorage.getItem(Application + "_Extensions")) || {
-    'box': 'corebox', 'arkham-asylum': true, 'wayne-manor': true, 'maps-pack': true
+    'corebox': 'true'
 };
